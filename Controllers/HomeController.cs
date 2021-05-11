04e3c809d4876690e9412b8a9319e28f513947b6
Cansel Yılmaz <62007618+CanselYilmaz@users.noreply.github.com>
2021-05-11 14:16:53 +0300

    Anasayfa Modülü Tamamlandı  - 9.Hafta
    Yorum ve Blog görüntülenmesi ayarlandı. Kullanıcı blog ve yorumları görüntüleyebilir.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,12 +5,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using knowledgenetwork.Models;
+using knowledgenetwork.Repositories.Interfaces;
+using knowledgenetwork.Repositories;
 
 namespace knowledgenetwork.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController(){}
+        private readonly ICommentRepository commentRepository;
+        private readonly IBlogRepository blogRepository;
+        private readonly ICategoryRepository catRepository;
+
+
+        public HomeController(ICommentRepository cRepo, IBlogRepository blogRepo, ICategoryRepository catRepo)
+        {
+            commentRepository = cRepo;
+            blogRepository = blogRepo;
+            catRepository = catRepo;
+        }
+        public HomeController() { }
 
         public IActionResult Index()
         {
@@ -20,6 +33,28 @@ namespace knowledgenetwork.Controllers
         public IActionResult BlogPost()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Blog()
+        {
+            return View(await blogRepository.GetAllAsync());
+        }
+
+        public async Task<IActionResult> BlogPage(int? id)
+        {
+            Blog blog = new Blog();
+            if (id != null)
+            {
+                blog = await blogRepository.GetByIdAsync(id.Value);
+            }
+            ViewBag.CategoryId = catRepository.GetDropboxList();
+            return View(blog);
+        }
+
+        public async Task<IActionResult> GetComments()
+        {
+            List<Comment> comments = await commentRepository.GetAllAsync();
+            return View(comments);
         }
     }
 }
