6345e636a7e8845189c26e188535225ea493983a
Cansel Yılmaz <62007618+CanselYilmaz@users.noreply.github.com>
2021-05-23 18:01:23 +0300

    Kullanıcı Yorumları Özelliği Ve Proje Tamamlandı - 11.Hafta
    Proje tamamnlandı.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,26 +13,12 @@ namespace knowledgenetwork.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ICommentRepository commentRepository;
-        private readonly IBlogRepository blogRepository;
-        private readonly ICategoryRepository catRepository;
-        private readonly ITagsRepository tagsRepository;
-
-
-        public HomeController(ICommentRepository cRepo, IBlogRepository blogRepo, ICategoryRepository catRepo, ITagsRepository tagsRepo)
+        public HomeController()
         {
-            commentRepository = cRepo;
-            blogRepository = blogRepo;
-            catRepository = catRepo;
-            tagsRepository = tagsRepo;
         }
-        public HomeController() { }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            BlogPageDto blogPageDto = new BlogPageDto();
-            blogPageDto.Blogs = await blogRepository.GetAllAsync();
-            blogPageDto.Tags = await tagsRepository.GetAllAsync();
             return View();
         }
 
@@ -41,26 +27,19 @@ namespace knowledgenetwork.Controllers
             return View();
         }
 
-        public async Task<IActionResult> Blog()
+        public IActionResult Blog()
         {
-            return View(await blogRepository.GetAllAsync());
+            return View();
         }
 
-        public async Task<IActionResult> BlogPage(int? id)
+        public IActionResult BlogPage(int? id)
         {
-            Blog blog = new Blog();
-            if (id != null)
-            {
-                blog = await blogRepository.GetByIdAsync(id.Value);
-            }
-            ViewBag.CategoryId = catRepository.GetDropboxList();
-            return View(blog);
+            return View();
         }
 
-        public async Task<IActionResult> GetComments()
+        public IActionResult GetComments()
         {
-            List<Comment> comments = await commentRepository.GetAllAsync();
-            return View(comments);
+            return View();
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,33 @@
+# Bilgi Ağı Blog Sitesi 
+
+![](resimler/logos.png)
+- Bilgi Ağı Asp Net Core , Entityframework Core ve MSSQL veritabanı kullanılarak geliştirilmiştir.
+
+- Kullanıcılar sisteme kayıt olup, giriş yapabilir.
+![](resimler/girisekrani.PNG)
+![](resimler/kayitekrani.PNG)
+
+
+- Kullanıcılar anasayfada yazarların paylaşmış olduğu blogları görüntülüyebilir, arayabilir ve inceleyebilir.
+
+![](resimler/anasayfa.PNG)
+![](resimler/blogekrani.PNG)
+
+- Kullancılar bloglara yorum yapabilir.
+
+![](resimler/yorumlar.png)
+
+- Yönetici sisteme yazar ekleyip yazarları yönetebilir.
+
+![](resimler/yazarlar.png)
+![](resimler/yazarekle.png)
+
+- Yönetici aynı panelden kategori yönetimi yapabilir.
+
+![](resimler/kategorilistesi.png)
+![](resimler/kategoriekle.png)
+
+- Yazarlar blog yönetimi yapabilir ve bloglarının okunma sayılarına bakabilir.
+![](resimler/blogekleme.png)
+![](resimler/bloglistesi.png)
+
